1abff2618e68207a0fcb539b5e8105229713bdfe
Jim-Hawkins <67739508+Jim-Hawkins@users.noreply.github.com>
2021-02-15 17:24:57 +0100

    Update tablero.py

--- tablero.py
+++ tablero.py
@@ -1,22 +1,5 @@
 class Tablero:
-    def __init__(self, rows, cols):
-        self.mapa = []
-        if rows >= 4:
-            self.rows = rows
-        else:
-            self.rows = 4
-        if cols >= 4:
-            self.cols = cols
-        else:
-            self.cols = 4
-
-        for i in range(rows):
-            self.mapa.append([])
-            for j in range(cols):
-                if i in (0, rows - 1):
-                    self.mapa[i].append(3)
-                else:
-                    if j in (0, cols - 1):
+    def __init__(sel
                         self.mapa[i].append(3)
                     else:
                         self.mapa[i].append(0)
