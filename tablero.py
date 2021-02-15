c36552873b139e5ebbe00c2e39a15f8ff95746e5
Jim-Hawkins <sergiogilnova2001@gmail.com>
2021-02-15 16:54:05 +0100

    Archivo .gitignore y módulo tablero añadidos.

---
+++ tablero.py
@@ -0,0 +1,35 @@
+class Tablero:
+    def __init__(self, rows, cols):
+        self.mapa = []
+        if rows >= 4:
+            self.rows = rows
+        else:
+            self.rows = 4
+        if cols >= 4:
+            self.cols = cols
+        else:
+            self.cols = 4
+
+        for i in range(rows):
+            self.mapa.append([])
+            for j in range(cols):
+                if i in (0, rows - 1):
+                    self.mapa[i].append(3)
+                else:
+                    if j in (0, cols - 1):
+                        self.mapa[i].append(3)
+                    else:
+                        self.mapa[i].append(0)
+
+    def __str__(self):
+        res = ''
+        for i in range(self.rows):
+            for j in range(self.cols):
+                res += str(self.mapa[i][j])
+            res += '\n'
+
+        return res
+
+
+c = Tablero(15, 20)
+print(c)
