f7010bfc21f8833bbf7cfeb0774a2d5916cca00f
Jim-Hawkins <sergiogilnova2001@gmail.com>
2021-02-16 10:36:52 +0100

    Tablero restaurado

--- tablero.py
+++ tablero.py
@@ -1,5 +1,22 @@
 class Tablero:
-    def __init__(sel
+    def __init__(self, rows, cols):
+        self.mapa = []
+        if rows >= 4:
+            self.rows = rows
+        else:
+            self.rows = 4
+        if cols >= 4:
+            self.cols = cols
+        else:
+            self.cols = 4
+
+        for i in range(rows):
+            self.mapa.append([])
+            for j in range(cols):
+                if i == 0 or i == rows - 1:
+                    self.mapa[i].append(3)
+                else:
+                    if j == 0 or j == cols - 1:
                         self.mapa[i].append(3)
                     else:
                         self.mapa[i].append(0)
@@ -14,17 +31,5 @@ class Tablero:
         return res
 
 
-x = input("Introduce el número de filas del tablero ")
-y = input("Introduce el número de columnas del tablero ")
-c = Tablero(x, y)
-
-
-for i in range(2):
-    print(c)
-
-print("Hola mundo")
-
-jgvjbviebmoebi
-bmebke`wbm
-bme`wbéw
-be
+c = Tablero(15, 20)
+print(c)
